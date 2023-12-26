5ab6032e18c69d7b42654dcec04dff07f9a15b9d
nejmiyassine <devtwins0@gmail.com>
2023-12-26 13:06:18 +0100

    Add shadows to the cards

--- src/components/Skills/Skills.style.js
+++ src/components/Skills/Skills.style.js
@@ -60,6 +60,7 @@ export const SkillsGroup = styled.div`
 export const SkillsData = styled.div`
     display: flex;
     column-gap: 0.5rem;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 
     & svg {
         color: ${({ theme }) => theme.firstColor};
